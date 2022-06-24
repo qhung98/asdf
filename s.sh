dff1ad71d8a86aaef387f71518bc5a4adacb79fe
root <root@vmi900325.contaboserver.net>
2022-06-24 17:53:01 +0100

    a1

--- s.sh
+++ s.sh
@@ -2,8 +2,8 @@
 
 time=$(date +%s)
 echo $time >> /root/t/a.txt
-git add /root/t >> /tmp/e
-git commit -m 'a1' >> /tmp/e
-git push -u origin main >> /tmp/e
+git -C /root/t add . >> /tmp/e
+git -C /root/t commit -m 'a1' >> /tmp/e
+git -C /root/t push -u origin main >> /tmp/e
 echo $time >> /tmp/t.txt
 
